984921bb0116c56bb4b9704b5cb592b969008829
jangyeonu <116713543+JangYeonWoo@users.noreply.github.com>
2025-04-14 15:43:55 +0900

    [Silver V] Title: 나이순 정렬, Time: 1748 ms, Memory: 117832 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String [][] arr = new String [n][2];
+
+        for (int i=0; i<n; i++) {
+            arr[i][0] = sc.next();
+            arr[i][1] = sc.next();
+        }
+
+        Arrays.sort(arr, Comparator.comparing(a -> Integer.parseInt(a[0])));
+
+        for (int i=0; i<arr.length; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+}
